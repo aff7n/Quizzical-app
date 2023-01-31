9f084a173b18ad59c4dfc66af2b25e7cc41d0914
Affan Khan <81604943+aff7n@users.noreply.github.com>
2023-01-31 17:11:31 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./App.css";
+import "./responsive.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -8,7 +10,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
